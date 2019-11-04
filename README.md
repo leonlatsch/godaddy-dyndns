002767b83bc62fc389e8da1adae8ece1e6250131
Leon Latsch <dev.leonlatsch@gmail.com>
2019-11-04 20:50:12 +0100

    #2 - added cached ip for only updating dns when ip has changed

--- README.md
+++ README.md
@@ -5,14 +5,15 @@ Python based GoDaddy dyndns script using only the GoDaddy api.
 ## Functionallity
 
 - Gets the public ip using [api.ipify.org](https://api.ipify.org/?format=raw). Fallback: [ip.42.pl](http://ip.42.pl/raw)
-- Creates or updates the dns entry with the name of `@` and the type of `A` pointing to your ip.
+- Caches the last obtained ip and checks for a change.
+- Creates or updates the dns entry with the name of `@` and the type of `A` pointing to your ip in case of a new ip.
 - **Won't work if there are more than one entries matching this condition!** (very unlikely)
 
 ## Usage
 
 - Clone the repository.
-- Enter your api credentials and domain name into `godaddy-dyndns.conf`. __You can create api credentials [here](https://developer.godaddy.com).__
+- Enter your api credentials and domain name into `godaddy-dyndns.conf`. **You can create api credentials [here](https://developer.godaddy.com).**
 - run the script to test it (You may have to add execution permissions).
-- I reccomand setting up a cron job to update the dns entry every hour: `0 * * * * /path/to/script.py`
+- I recommend setting up a cron job to run the script every hour: `0 * * * * /path/to/script.py`. **[Help](https://crontab.guru)**
 
 If you have questions feel free to create an issue.
--- godaddy-dyndns.py
+++ godaddy-dyndns.py
@@ -6,8 +6,9 @@
 
 from configparser import ConfigParser
 import requests
-import json
 import os
+
+LAST_IP = os.path.dirname(__file__) + "/.last-ip"
 
 cfg = ConfigParser()
 cfg.read(os.path.dirname(__file__) + "/godaddy-dyndns.conf")
@@ -19,6 +20,20 @@ domain = cfg.get("godaddy", "domain")
 base_url = "https://api.godaddy.com"
 endpoint_update = "/v1/domains/" + domain + "/records"
 endpoint_records = "/v1/domains/" + domain + "/records/A/@"
+
+def safe_new_ip(ip):
+    f = open(LAST_IP, "w")
+    f.write(ip)
+    f.close()
+
+def get_last_ip():
+    try:
+        f = open(LAST_IP)
+        ip = f.read()
+        f.close()
+        return ip
+    except FileNotFoundError:
+        return None
 
 def get_ip():
     r = requests.get("https://api.ipify.org/?format=raw")
@@ -42,11 +57,21 @@ def update_dns(ip):
         return  requests.put(base_url + endpoint_records, json=new_records, headers=headers)
     elif len(records) > 1:
         print("[!] You got " + str(len(records)) + " records of type A with host @. Please delete as least " + str(len(records) - 1) + " of them")
-        
 
-ip = get_ip()
-r = update_dns(ip)
-if r is not None and r.status_code == 200:
-    print("[*] Updated dns record for " + domain + " to " + ip)
-elif r is not None:
-    print("[!] Error updating dns record: " + str(r.status_code) + " " + str(r.reason))
+
+def main():
+    last_ip = get_last_ip()
+    ip = get_ip()
+
+    if ip != last_ip:
+        safe_new_ip(ip)
+        r = update_dns(ip)
+        if r is not None and r.status_code == 200:
+            print("[*] Updated dns record for " + domain + " to " + ip)
+        elif r is not None:
+            print("[!] Error updating dns record: " + str(r.status_code) + " " + str(r.reason))
+    else:
+        print("[*] Ip hasn't changed, no update")
+
+if __name__ == "__main__":
+    main()
